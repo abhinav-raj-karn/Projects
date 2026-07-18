d3b8b2588e629e07ead03e6a8afff32570293ca7
abhinav-raj-karn <karnpariwar@gmail.com>
2026-07-18 19:58:42 +0530

    intitial commits

---
+++ dirsize/dirsize.c
@@ -0,0 +1,29 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+//this is the custom function to sum up
+//all the file size
+
+void add_to_firlesize(const char *fpath, const struct stat *sb,
+             int typeflag, struct FTW *ftwbuf)
+{
+    printf("filename: %s\n", fpath);
+
+}
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+      printf("usage: %s <dirname>\n", argv[0]);
+      exit (0);
+  }
+
+  // find the size of the entire directory
+  // 1. get directory dirname
+  char *dirname = argv[1];
+  printf("Dirname: %s\n", dirname);
+
+  nftw(dirname, add_to_firlesize,
+
+  return 0;
+}
