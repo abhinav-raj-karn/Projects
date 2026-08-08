e75cdb74d138be693d52cdd6dfe2ab4223885f4c
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-08 21:14:42 +0530

    Update

---
+++ c-programming/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  // this is a comment
+  // test comment lala
+  printf("test hello\n");
+
+  // variable = A resuable container for value.
+  //            Behaves as if it ere the value it contains.
+
+  int age = 18;
+  int year = 2026;
+  printf("You are %d years old\n", age);
+  printf("The year is %d.\n", year);
+  return 0;
+}
