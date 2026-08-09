77add40bcc39427512c74061533b7c0d3397f892
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-09 15:08:34 +0530

    Update

---
+++ c-programming/a-op.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+ 
+  // arithmetic operators = + - * / % ++ --
+
+  int x = 2;
+  float y = 3;
+  float z = 0;
+
+  // z = x + y;
+  // z = x - y;
+  // z = x * y;
+  z = x / y;
+  printf("%f\n", z);
+
+  return 0;
+
+}
