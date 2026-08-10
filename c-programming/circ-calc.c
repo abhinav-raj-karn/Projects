e68d1c4c1daed1cc8456f007c7061e64af4dfca3
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-10 21:02:55 +0530

    Update

--- c-programming/circ-calc.c
+++ c-programming/circ-calc.c
@@ -4,5 +4,14 @@
 
 int main()
 {
+  double radius = 0.0;
+  double area = 0.0;
+  const double PI = 3.14159;
 
+  printf("Enter the radius: ");
+  scanf("%lf", &radius);
+
+  area = PI * pow(radius, 2);
+  printf("Area: %.2lf", area);
+  return 0;
 }
