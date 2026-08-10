b7938a95f21c03910c1ebefdcef4f7665b4108b9
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-10 17:04:08 +0530

    Update

--- c-programming/math.c
+++ c-programming/math.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 
 int main()
 {
-  int x = 9;
-
-  printf("%d\n", x);
-
+  float x = 45;
   
+  //x = sqrt(x); // square root
+  //x = pow(x, 3);  // power of 
+  //x = round(x);  // round of
+  //x = ceil(x);  // ceiling rounded UP
+  //x = floor(x);  // floor rounded DOWN
+  //x = abs(x);   // absolute value
+  //x = log(x);  // finds logarithma
+  //x = sin(x);  //sine
+  //x = cos(x);  // cosine
+  //x = tan(x);  // tangent
+  printf("%f", x);
+
   return 0;
 }
