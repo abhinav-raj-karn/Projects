665fa01e178ed090f9750f6d462d7e8c84a01df3
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-10 21:11:51 +0530

    Update

--- c-programming/circ-calc.c
+++ c-programming/circ-calc.c
@@ -6,12 +6,19 @@ int main()
 {
   double radius = 0.0;
   double area = 0.0;
+  double surfaceArea = 0.0;
+  double volume = 0.0;
   const double PI = 3.14159;
 
   printf("Enter the radius: ");
   scanf("%lf", &radius);
 
   area = PI * pow(radius, 2);
-  printf("Area: %.2lf", area);
+  surfaceArea = 4 * PI * pow(radius, 2);
+  volume = (4.0 / 3.0) * PI * pow(radius, 3);
+
+  printf("Area: %.2lf\n", area);
+  printf("surfaceArea: %.2lf\n", surfaceArea);
+  printf("Volume: %.2lf\n", volume);
   return 0;
 }
