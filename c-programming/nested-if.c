90f13fe1387bf0a36c4a727700750dcab733b492
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-11 09:30:41 +0530

    Update

---
+++ c-programming/nested-if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+  float price = 10.00;
+  bool isStudent = false;
+
+  if (isStudent){
+    printf("You get a student discount of 10%\n");
+    price *= 0.9;
+  }
+  printf("The price of ticket will be %.2f\n", price);
+}
