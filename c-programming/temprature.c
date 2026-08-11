b5b980e7dd1ade5f76de9bda1981fd02d8a76b9f
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-11 08:47:00 +0530

    Update

--- c-programming/temprature.c
+++ c-programming/temprature.c
@@ -13,9 +13,22 @@ int main()
   printf("Is the temp in Celsius (C) or Fahrenheit (F):  ");
   scanf("%c", &choice);
 
+
   if (choice == 'C'){
     //C to F
-
+    printf("Enter celsius:  ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32; // C to F
+    printf("%.1f°C is %.1f°F", celsius, fahrenheit);
+  }
+  else if (choice == 'F'){
+    printf("Enter fahrenheit:  ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32) * 5 / 9;  //F to C
+    printf("%.1f°F is %.1f°C", fahrenheit, celsius);
+  }
+  else{
+    printf("Invalid choice! please select C or F\n");
   }
   return 0;
 }
