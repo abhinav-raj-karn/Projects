6e96f475bdf90a09ada9bdf8d9848e40eb7e2aff
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-11 08:54:28 +0530

    Update

--- c-programming/tempreture.c
+++ c-programming/tempreture.c
@@ -7,21 +7,21 @@ int main()
   float fahrenheit = 0.0f;
   float celsius = 0.0f;
 
-  printf("TEMPRATURE CONVERSION PROGRAM\n");
+  printf("TEMPRETURE CONVERSION PROGRAM\n");
   printf("C. Celsius to Fahrenheit\n");
   printf("F. Fahrenheit to Celsius\n");
   printf("Is the temp in Celsius (C) or Fahrenheit (F):  ");
   scanf("%c", &choice);
 
 
-  if (choice == 'C'){
+  if (choice == 'C' || choice == 'c'){
     //C to F
     printf("Enter celsius:  ");
     scanf("%f", &celsius);
     fahrenheit = (celsius * 9 / 5) + 32; // C to F
     printf("%.1f°C is %.1f°F", celsius, fahrenheit);
   }
-  else if (choice == 'F'){
+  else if (choice == 'F' || choice == 'f'){
     printf("Enter fahrenheit:  ");
     scanf("%f", &fahrenheit);
     celsius = (fahrenheit - 32) * 5 / 9;  //F to C
