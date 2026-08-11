3fa748e15a88f51053564db30fdf30f3bf2cbb7a
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-11 07:02:26 +0530

    Update

---
+++ c-programming/temprature.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  // TEMPRATURE CONVERSION PROGRAM
+  char choice = '\0';
+  float fahrenheit = 0.0f;
+  float celsius = 0.0f;
+
+  printf("TEMPRATURE CONVERSION PROGRAM\n");
+  printf("C. Celsius to Fahrenheit\n");
+  printf("F. Fahrenheit to Celsius\n");
+  printf("Is the temp in Celsius (C) or Fahrenheit (F):  ");
+  scanf("%c", &choice);
+
+  if (choice == 'C'){
+    //C to F
+
+  }
+  return 0;
+}
