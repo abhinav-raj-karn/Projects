64a1a1453fff3d1b647b8cd20c34965727c8ef57
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-11 14:04:03 +0530

    Update

--- c-programming/nested-if.c
+++ c-programming/nested-if.c
@@ -3,12 +3,27 @@
 
 int main () {
   float price = 10.00;
-  bool isStudent = false;  // 10% discount
-  bool isSenior = true; //
+  bool isStudent = true;  // 10% discount
+  bool isSenior = false; // 20% discount
+
+  // student = $9
+  // senior = $8
+  // student + senior = $7
 
   if (isStudent){
+    if (isSenior){
+      printf("You get a student discount of 10%\n");
+      printf("You get a senior discount of 20%\n");
+      price *= 0.7;
+    }
+    else {
+      printf("You get a student discount of 10%\n");
+      price *= 0.9;
+    }
+  }
+  else {
     printf("You get a student discount of 10%\n");
-    price *= 0.9;
+    price *= 0.8;
   }
   printf("The price of ticket will be $%.2f\n", price);
 }
