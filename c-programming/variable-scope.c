acd694039e23a3be65b4d2d318dca6dc76f11afe
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 16:06:47 +0530

    Update

--- c-programming/variable-scope.c
+++ c-programming/variable-scope.c
@@ -10,10 +10,14 @@ int add(int x, int y){
   return result;
 }
 
+int subtract(int x, int y){
+  int result = x - y;
+  return result;
+}
 
 int main(){
  
-  int result = add(3, 4);
+  int result = subtract(3, 4);
   
   printf("%d\n", result);
 
