b0990f461d1500fd46e66e971430b1170f50d724
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 16:31:41 +0530

    Update

---
+++ c-programming/func-prototypes.c
@@ -0,0 +1,37 @@
+// function prototypes = Provide the compiler w/ information about a function's:
+//                       name, return type, and parameters before its actual definition.
+//                       Enables tupe checking and allows functions to be used before
+//                       they're defined.
+//                       Inproves readibility, organization, and helps prevent errors.
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void hello(char name[], int age);  // function prototype
+bool ageCheck(int age);
+
+
+int main()
+{
+
+  hello("Calm", 30);
+  if(ageCheck(30)){
+    printf("You are old enough to work at Krusty Krab\n");
+  }
+  else{
+    printf("You must be 16+ to work at Krusty Krab\n");
+  }
+
+  return 0;
+}
+
+void hello(char name[], int age)
+{
+  printf("Hello %s\n", name);
+  printf("You are %d years old\n", age);
+}
+
+bool ageCheck(int age){
+  return age >= 16;
+}
+
