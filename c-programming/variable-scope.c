1cd91c3859c5c24b862560fa0a788b0f34c2e313
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 16:03:58 +0530

    Update

---
+++ c-programming/variable-scope.c
@@ -0,0 +1,21 @@
+// variable scope = refers to where a variable is recognized and accessible.
+//                  variable can share the same name if 
+//                  they're in diffent scopes {}
+
+
+#include <stdio.h>
+
+int add(int x, int y){
+  int result = x + y;
+  return result;
+}
+
+
+int main(){
+ 
+  int result = add(3, 4);
+  
+  printf("%d\n", result);
+
+  return 0;
+}
