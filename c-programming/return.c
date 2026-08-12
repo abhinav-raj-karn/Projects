815c6d6c29c7b2759b1107cc63be2f3ecaa0f474
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 11:18:30 +0530

    Update

---
+++ c-programming/return.c
@@ -0,0 +1,22 @@
+// return = returns a value back to where you call a function
+
+#include <stdio.h>
+
+int square(int num){
+  int result = num * num;
+
+  return result;
+}
+
+
+int main() {
+  int x = square(2);
+  int y = square(3);
+  int z = square(4);
+
+  printf("%d\n", x);
+  printf("%d\n", y);
+  printf("%d\n", z);
+
+  return 0;
+}
