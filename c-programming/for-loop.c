f0232bd71c8a678ef52f8151c6536fe765b2b415
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 20:56:31 +0530

    Update

---
+++ c-programming/for-loop.c
@@ -0,0 +1,15 @@
+// for loop = Repeat some code a limited # of times
+//            for(Initialization; Condition; Update)
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main (){
+
+  for(int i = 10; i >= 0; i--){
+    sleep(1);
+    printf("%d\n", i);
+  }
+  printf("HAPPY NEW YEAR!\n");
+  return 0;
+}
