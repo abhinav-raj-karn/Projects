ac328794a81bae46df7c94ffbde89289bbe11594
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 08:49:08 +0530

    Update

---
+++ c-programming/function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // function = A reusable section of code that can be invoked "called"
+  //            Arguments can be sent to a function so that it can use them
+  
+
+  return 0;
+}
