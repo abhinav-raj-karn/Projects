26c3c9875c468317672fd450cb0801db2d1772b8
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 11:29:25 +0530

    Update

--- c-programming/return.c
+++ c-programming/return.c
@@ -1,22 +1,52 @@
 // return = returns a value back to where you call a function
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int square(int num){
-  int result = num * num;
+/*
+double cube(double num){
+  return num * num * num;
+}
 
-  return result;
+double square(double num){
+  return num * num;
 }
 
 
 int main() {
-  int x = square(2);
-  int y = square(3);
-  int z = square(4);
+  double x = cube(2);
+  double y = cube(3);
+  double z = cube(4);
 
-  printf("%d\n", x);
-  printf("%d\n", y);
-  printf("%d\n", z);
+  printf("%lf\n", x);
+  printf("%lf\n", y);
+  printf("%lf\n", z);
+
+  return 0;
+}
+*/
+
+bool ageCheck(int age){
+  
+  if(age >= 18){
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+int main(){
+
+  int age = 12;
+
+  if(ageCheck(age)){
+    printf("You may sign up");
+  }
+  else {
+    printf("You must be 18+ to sign up");
+  }
 
   return 0;
 }
