439f9e1c805d5c9647ed8d899f1d6106ef306134
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 20:09:10 +0530

    Update

--- c-programming/while-loop.c
+++ c-programming/while-loop.c
@@ -1,16 +1,39 @@
 // while loop = Continue some code WHILE the condition remains true 
 //              Condition must be true for us to enter the loop
 
+//              WHILE LOOP ARE GOOD FOR USER INPUTS
+
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main() {
-   
+
+/*
 
   int number = 0;
 
-  while(number <= 0){
+  do{
     printf("Enter a number greater than 0:  ");
     scanf("%d", &number);
+  }while(number <= 0);
+
+*/
+
+  char name[50] = "";
+
+  printf("Enter your name: ");
+  fgets(name, sizeof(name), stdin);
+  name[strlen(name) - 1] = '\0';
+
+  while (strlen(name) == 0){
+
+    printf("Name cannot be enpty! Please enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
   }
+  printf("Hello, %s", name);
+
+
   return 0;
 }
