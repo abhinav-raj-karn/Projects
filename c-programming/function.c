ec2cab0a9ba041e111af697a6fe23a3879aaba0b
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-12 11:10:02 +0530

    Update

--- c-programming/function.c
+++ c-programming/function.c
@@ -1,9 +1,31 @@
-#include <stdio.h>
-
-int main() {
   // function = A reusable section of code that can be invoked "called"
   //            Arguments can be sent to a function so that it can use them
-  
 
+#include <stdio.h>
+#include <string.h>
+
+
+
+void happyBirthday(char name[50], int age){
+
+   printf("\nHappy birthday to you");
+   printf("\nHappy birthday to you");
+   printf("\nHappy birthday dear %s!", name);
+   printf("\nYou are %d years old!\n", age);
+}
+
+int main() {
+
+    char name[50] = "";
+    int age = 0;
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    happyBirthday(name, age);
   return 0;
 }
