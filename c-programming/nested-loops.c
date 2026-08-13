a823dbbacfc7f246743da9da87e91df1663b85a2
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-13 07:22:34 +0530

    Update

--- c-programming/nested-loops.c
+++ c-programming/nested-loops.c
@@ -2,10 +2,13 @@
 
 int main() {
 
-  for(int i = 1; i < 10; i++){
-    printf("%d ", i);
+ for (int i = 1; i < 4; i++){
+
+  for(int j = 1; j < 10; j++){
+    printf("%d ", j);
+    }
+    printf("\n");
+
   }
-
-
   return 0;
 }
