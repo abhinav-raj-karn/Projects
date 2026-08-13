0fe93c61ef005711202f67b7db3f7d7f29954903
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-13 07:57:24 +0530

    Update

--- c-programming/random-numbers.c
+++ c-programming/random-numbers.c
@@ -9,10 +9,10 @@ int main() {
  //  printf("%d\n", rand());   // random number 
  // printf("%d\n", RAND_MAX);   // maximum value
   
-  int min = 1;
-  int max = 6;
+  int min = 50;
+  int max = 100;
 
-  int randomNum = (rand() % max) + min;
+  int randomNum = (rand() % (max - min + 1)) + min;
 
   printf("%d\n", randomNum);
   return 0;
