68aae2ba72e983f0a7b420225680b20eb31c7556
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-13 13:22:49 +0530

    Update

---
+++ c-programming/rock-paper-scissors.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getComputerChoice();
+int getUserChoice();
+void checkWinner(int userChoice, int computerChoice);
+
+int main() {
+
+
+  return 0;
+}
+
+
+int getComputerChoice(){
+
+  return 0;
+}
+
+int getUserChoice(){
+
+  return 0;
+}
+
+void checkWinner(int userChoice, int computerChoice){
+
+  return 0;
+}
+
+
