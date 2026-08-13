715472b6bff1d659c7fcc9eb17fe2db4097cd104
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-13 20:37:58 +0530

    Update

--- c-programming/rock-paper-scissors.c
+++ c-programming/rock-paper-scissors.c
@@ -7,6 +7,37 @@ int getUserChoice();
 void checkWinner(int userChoice, int computerChoice);
 
 int main() {
+  
+  srand(time(NULL));
+
+  printf("*** ROCK PAPER SCISSORS ***\n");
+
+  int userChoice = getUserChoice();
+  int computerChoice = getComputerChoice();
+
+  switch(computerChoice){
+    case 1:
+      printf("computer chose ROCK\n");
+      break;
+    case 2:
+      printf("computer chose PAPER\n");
+      break;
+    case 3:
+      printf("computer chose SCISSORS\n");
+      break;
+  }
+  switch(userChoice){
+    case 1:
+      printf("you chose ROCK\n");
+      break;
+    case 2:
+      printf("you chose PAPER\n");
+      break;
+    case 3:
+      printf("you chose SCISSORS\n");
+      break;
+  }
+
 
 
   return 0;
@@ -15,17 +46,28 @@ int main() {
 
 int getComputerChoice(){
 
-  return 0;
+  return (rand() % 3) + 1;
 }
 
 int getUserChoice(){
 
-  return 0;
+  int choice = 0;
+
+  do{
+    printf("Choose an option\n");
+    printf("1. ROCK\n");
+    printf("2. PAPER\n");
+    printf("3. SCISSORS\n");
+    printf("Enter your choice:  ");
+    scanf("%d", &choice);
+  }while(choice < 1 || choice > 3);
+
+  return choice;
 }
 
-void checkWinner(int userChoice, int computerChoice){
 
-  return 0;
+void checkWinner(int userChoice, int computerChoice){
+ return;
 }
 
 
