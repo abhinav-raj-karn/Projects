1c1a810fc3e086898c8e7c219dfb92d9a6c29039
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-14 20:41:17 +0530

    Update

---
+++ self-practice/simple-billing-system.c
@@ -0,0 +1,27 @@
+/*
+
+1. Simple billing system (best next project)
+Menu:
+Add item price
+View current bill
+Apply discount
+Checkout
+Exit
+
+*/
+
+
+#include <stdio.h>
+
+
+
+int main() {
+// Add item price
+  float item1 = 0.0f;
+  float item2 = 0.0f;
+  flaot item3 = 0.0f;
+  float total = 0.0f;
+
+
+
+}
