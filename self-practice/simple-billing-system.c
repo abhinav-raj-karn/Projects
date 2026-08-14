5fde791ea3eca059315fabcbbcbf95812a3f6de3
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-14 21:02:12 +0530

    Update

--- self-practice/simple-billing-system.c
+++ self-practice/simple-billing-system.c
@@ -19,9 +19,22 @@ int main() {
 // Add item price
   float item1 = 0.0f;
   float item2 = 0.0f;
-  flaot item3 = 0.0f;
+  float item3 = 0.0f;
   float total = 0.0f;
 
+  printf("Enter the price of items\n");
+  printf("ITEM 1: ");
+  scanf("%f", &item1);
 
+  printf("ITEM 2: ");
+  scanf("%f", &item2);
 
+  printf("ITEM 3: ");
+  scanf("%f", &item3);
+
+  total = item1 + item2 + item3;
+
+  printf("%f", total);
+
+  return 0;
 }
