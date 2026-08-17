d1263a87dc7785064046fccdd8a121ffc5274693
abhinav-raj-karn <karnpariwar@gmail.com>
2026-08-17 07:19:20 +0530

    Update

---
+++ c-programming/arrays.c
@@ -0,0 +1,20 @@
+// array = A fixed-size collection of elements of the same data type
+//         (Similar to a variable, but it holds more than 1 value)
+
+#include <stdio.h>
+
+int main() {
+
+
+  int numbers[] = {10, 20, 30, 40, 50};
+
+  printf("%d\n", numbers[0]);
+
+  char grades[] = {'A', 'B', 'C', 'D', 'E'};
+
+  printf("%c", grades[1]);
+
+  return 0;
+}
+
+
